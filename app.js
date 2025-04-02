1f4c709fd042ad7b230c3a61249106ca8e626345
Mary Dawn Malida <157961578+PseuDawncode@users.noreply.github.com>
2025-04-02 10:54:18 +0200

    added Math.floor for a randomNum

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const min = 50;
+/*const min = 50;
 const max = 100;
 
 let randomNum = Math.floor(Math.random() * (max - min)) + min;
 
-console.log(randomNum);
+console.log(randomNum);*/
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max =100;
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
